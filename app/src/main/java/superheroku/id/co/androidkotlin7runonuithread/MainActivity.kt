209c68d89d1738d8ea6c9471bfcbce7684e875fc
Programmer Muslim <rizki@imastudio.co.id>
2018-04-24 21:36:35 +0700

    add

--- app/src/main/java/superheroku/id/co/androidkotlin7runonuithread/MainActivity.kt
+++ app/src/main/java/superheroku/id/co/androidkotlin7runonuithread/MainActivity.kt
@@ -2,11 +2,26 @@ package superheroku.id.co.androidkotlin7runonuithread
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // start some dummy thread that is different from UI thread
+        Thread(Runnable {
+            // performing some dummy time taking operation
+            var i=0;
+            while(i<Int.MAX_VALUE){
+                i++
+            }
+
+            // try to touch View of UI thread
+            this@MainActivity.runOnUiThread(java.lang.Runnable {
+                this.textview_msg.text = "Updated from other Thread"
+            })
+        }).start()
     }
 }
